00c305d729b9204b246aa8b9785449be8e5db0c0
hernany <hernanytec777@gmail.com>
2023-01-09 22:04:37 -0300

    create home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Cover } from '../../components/Cover'
+import { HomeContainer } from './styles'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <Cover />
+    </HomeContainer>
+  )
+}
